3f155ac8a839d06bdfcfc441cd57e63f9eb87741
uwx <maxine@cybergal.com>
2025-01-09 12:27:11 -0300

    Separate out rspack-userscript-plugin

--- rspack.config.cjs
+++ rspack.config.cjs
@@ -1,58 +1,6 @@
 // @ts-check
 
-const rspack = require('@rspack/core');
 const { name, version, description, author } = require('./package.json');
-const fs = require('node:fs');
-const path = require('node:path');
-
-/**
- * @param {Record<string, string | string[]>} keyValues 
- */
-function generateUserscriptHeader(keyValues) {
-    /** @type {string[]} */
-    const lines = ['// ==UserScript=='];
-
-    for (const [key, values] of Object.entries(keyValues)) {
-        if (typeof values === 'string') {
-            lines.push(`// @${key.padEnd(11, ' ')} ${values}`);
-        } else {
-            for (const value of values) {
-                lines.push(`// @${key.padEnd(11, ' ')} ${value}`);
-            }
-        }
-    }
-
-    lines.push('// ==/UserScript==');
-    lines.push('');
-
-    return lines.join('\n');
-}
-
-/** @type {Record<string, string | string[]>} */
-const userscriptHeader = {
-    name: name,
-    version: version,
-    author: author.name,
-    description: description,
-    include: [
-        'https://bsky.app/*',
-        'https://*.bsky.dev/*',
-    ],
-    grant: [
-        'GM_setValue',
-        'GM_getValue',
-        'GM_registerMenuCommand',
-        'GM_xmlhttpRequest',
-    ],
-};
-
-fs.writeFileSync(
-    `./dist/${name}.proxy.user.js`,
-    generateUserscriptHeader({
-        ...userscriptHeader,
-        require: `file://${path.resolve(`dist/${name}.user.js`)}`
-    })
-);
 
 /** @type {import('@rspack/cli').Configuration} */
 module.exports = {
@@ -85,9 +33,24 @@ module.exports = {
         ],
     },
     plugins: [
-        new rspack.BannerPlugin({
-            banner: generateUserscriptHeader(userscriptHeader),
-            raw: true,
+        require('./rspack-userscript-plugin.cjs')({
+            name,
+            header: {
+                name: name,
+                version: version,
+                author: author.name,
+                description: description,
+                include: [
+                    'https://bsky.app/*',
+                    'https://*.bsky.dev/*',
+                ],
+                grant: [
+                    'GM_setValue',
+                    'GM_getValue',
+                    'GM_registerMenuCommand',
+                    'GM_xmlhttpRequest',
+                ],
+            }
         })
     ],
     optimization: {
